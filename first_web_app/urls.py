19b3f41f09ae90cd59222f2fdf95f1bb95434997
Sanchit Jain <sanchit.jain@mail.mcgill.ca>
2019-02-28 15:44:56 -0500

    Home page commit

--- first_web_app/urls.py
+++ first_web_app/urls.py
@@ -14,8 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.http import HttpResponse
 from django.urls import path
 
+def home_page(request):
+    return HttpResponse("Hi this is the home page")
+
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('home', home_page)
 ]
